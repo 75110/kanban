1f2d81d373f59ebe0d36e490532c7468f6e31f23
sevenioi <05110tv@gmail.com>
2025-08-26 16:08:34 +0800

    时间改中文

--- client/src/main.js
+++ client/src/main.js
@@ -6,6 +6,7 @@ import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 import './style/index.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 const app = createApp(App)
 
@@ -16,6 +17,8 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
 
 app.use(createPinia())
 app.use(router)
-app.use(ElementPlus)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
 
 app.mount('#app')
